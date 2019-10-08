b641b01c6e6d90cac1c06f1d36c326ac03c3ad5d
PJ Quirk <pjquirk@jubei.northamerica.corp.microsoft.com>
2019-10-07 22:22:54 -0400

    Initial version

---
+++ dist/index.js
@@ -0,0 +1,49 @@
+"use strict";
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const Core = __importStar(require("@actions/core"));
+const github_1 = require("@actions/github");
+const FeatureFlagLabelName = "Feature Flag Rollout";
+async function run() {
+    const actionToken = Core.getInput('action-token');
+    const github = new github_1.GitHub(actionToken);
+    console.log(`Reading issue ${github_1.context.issue.number} from ${github_1.context.issue.owner}/${github_1.context.issue.repo}`);
+    const issue = await github.issues.get({
+        number: github_1.context.issue.number,
+        owner: github_1.context.issue.owner,
+        repo: github_1.context.issue.repo
+    });
+    const hasFeatureFlagLabel = issue.data.labels.some(label => label.name === FeatureFlagLabelName);
+    if (!hasFeatureFlagLabel) {
+        console.log(`Issue is not labelled with '${FeatureFlagLabelName}, ignoring.`);
+        return;
+    }
+    // Extract the FF name from the title. The title is expected to be:
+    //   "Roll out FEATURE_NAME"
+    const match = issue.data.title.match(/^Roll out (.*)/);
+    if (!match || match.length == 0) {
+        console.log(`No feature name was found in the title, ignoring.`);
+        return;
+    }
+    const featureName = match[0];
+    console.log(`Found issue for feature ${featureName}`);
+    // Extract the FF status from the body.  The body is expected to be (in Markdown):
+    //
+    // - [X] Stage Name 0
+    // - [ ] Stage Name 1
+    // - [ ] Stage Name 2
+    const pathToStatusPage = Core.getInput('path-to-status-page');
+    console.log(`Writing status to: ${pathToStatusPage}`);
+}
+run()
+    .then((response) => { console.log(`Finished running: ${response}`); }, (error) => {
+    console.log(`#ERROR# ${error}`);
+    Core.setFailed(error.message);
+});
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,52 @@
+import * as Core from "@actions/core";
+import { context, GitHub } from "@actions/github";
+
+const FeatureFlagLabelName = "Feature Flag Rollout";
+
+async function run() {
+    const actionToken = Core.getInput('action-token');
+    const github = new GitHub(actionToken);
+    
+    console.log(`Reading issue ${context.issue.number} from ${context.issue.owner}/${context.issue.repo}`);
+    const issue = await github.issues.get({
+        number: context.issue.number,
+        owner: context.issue.owner,
+        repo: context.issue.repo
+    });
+
+    const hasFeatureFlagLabel = issue.data.labels.some(label => label.name === FeatureFlagLabelName);
+    if (!hasFeatureFlagLabel) {
+        console.log(`Issue is not labelled with '${FeatureFlagLabelName}, ignoring.`);
+        return;
+    }
+
+    // Extract the FF name from the title. The title is expected to be:
+    //   "Roll out FEATURE_NAME"
+    const match = issue.data.title.match(/^Roll out (.*)/);
+    if (!match || match.length == 0) {
+        console.log(`No feature name was found in the title, ignoring.`);
+        return;
+    }
+    const featureName = match[0];
+    console.log(`Found issue for feature ${featureName}`)
+
+    // Extract the FF status from the body.  The body is expected to be (in Markdown):
+    //
+    // - [X] Stage Name 0
+    // - [ ] Stage Name 1
+    // - [ ] Stage Name 2
+    
+
+    const pathToStatusPage = Core.getInput('path-to-status-page');
+    console.log(`Writing status to: ${pathToStatusPage}`);
+    
+}
+
+run()
+    .then(
+        (response) => { console.log(`Finished running: ${response}`) },
+        (error) => { 
+            console.log(`#ERROR# ${error}`);
+            Core.setFailed(error.message);
+        }
+    )
