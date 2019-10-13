d19545a73109c982a93b53cb209e79997b88a3de
PJ Quirk <pjquirk@jubei.northamerica.corp.microsoft.com>
2019-10-13 15:57:33 -0400

    ugh

--- dist/index.js
+++ dist/index.js
@@ -74,7 +74,7 @@ async function run() {
         console.log(`${pathToStatusPage} found.`);
     }
     catch (e) {
-        if (!e || !e.message || e.message === "Not Found") {
+        if (!e || !e.message || e.message !== "Not Found") {
             console.log(`Failed to retrieve the status page contents: ${e.message}`);
             Core.setFailed(e.message);
             return;
